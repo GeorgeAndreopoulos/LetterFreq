3ecfb145b20d292d926dd279d021be49032d7bad
GeorgeAndreopoulos <george.andreo@outlook.com>
2026-04-24 02:25:43 +0300

    set map with letter counts to default

--- frequencyCount.rs
+++ frequencyCount.rs
@@ -1,6 +1,14 @@
-use std::env;
+std::env;
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
-    
+    let mut lettersCount: HashMap<char,i32> = HashMap::new();
+    let mut beginChar = 'a';
+    lettersCount.insert('a',0);
+    for i in 1..26{
+       beginChar = ((beginChar as u8) + 1) as char;
+       lettersCount.insert(beginChar,0);
+    }
+    println!("{:?}", lettersCount);
 }
